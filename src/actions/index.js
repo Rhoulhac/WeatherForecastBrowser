53fd5068026da16418f62f373bcad8550b8e7c15
Rhoulhac <greg.rhoulhac@gmail.com>
2018-02-22 07:40:50 -0600

    Use Redux thunk and add alert on error

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,16 +2,26 @@ import axios from 'axios';
 
 const API_KEY = config.API_KEY;
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
-
+const fetchWeatherAction = (payload) => ({
+  type: FETCH_WEATHER,
+  payload: payload
+})
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
-  const url = `${ROOT_URL}&q=${city},us`;
-  // axios returns a promise (doesn't actually contain any data)
-  const request = axios.get(url);
+  return (dispatch, getState) => {
+    const url = `${ROOT_URL}&q=${city},us`;
+    // axios returns a promise (doesn't actually contain any data)
+    const request = axios.get(url);
 
-  return {
-    type: FETCH_WEATHER,
-    payload: request
-  };
+    request.then((payload) => {
+      dispatch(fetchWeatherAction(payload));
+    }, (error) => {
+      if (error.request.status == 404){
+        alert("City not found")
+      } else {
+        alert("An error has occured")
+      }
+    })
+  }
 }
